6316366ac56396721d034c9383f5896f2c477ad0
ALI RAZA MUJAHID <razadeveloper816@gmail.com>
2026-04-29 10:25:47 -0700

    Add header, CSS styles, and initial HTML structure
    - Created main.css for styling with a Google font and basic reset.
    - Added header.php for the navigation structure with links to Home, Collection, About, and Contact.
    - Included an image logo in the header.
    - Created index.html with a button and linked to the CSS.
    - Added index.php to include the header for the main page.

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="container">
+                <div class="col-2 border">
+                    <img src="./images/InsiderStats.png" class='img-fluid' alt="">
+                </div>
+                <div class="col-6 border d-flex justify-content-center align-items-center">
+                    <ul class="nav">
+                        <li class="nav-item"><a class="nav-link pages" href="#">Home</a></li>
+                        <li class="nav-item"><a class="nav-link pages" href="#">Collection</a></li>
+                        <li class="nav-item"><a class="nav-link pages" href="#">About</a></li>
+                        <li class="nav-item"><a class="nav-link pages" href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-2 border"></div>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>

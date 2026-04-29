e97124f21e6712ee7d7d9443e5e608110a6845c2
ALI RAZA MUJAHID <razadeveloper816@gmail.com>
2026-04-29 10:49:05 -0700

    Add FontAwesome icons, update header layout, and enhance index page content

--- header.php
+++ header.php
@@ -4,18 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- BOOTSTRAP  -->
     <link rel="stylesheet" href="./css/bootstrap.css">
     <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <!-- MAIN CSS FILE  -->
     <link rel="stylesheet" href="./css/main.css">
+    <!-- FONTAWESOME CDN  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <header>
         <nav class="navbar">
             <div class="container">
-                <div class="col-2 border">
+                <div class="col-2">
                     <img src="./images/InsiderStats.png" class='img-fluid' alt="">
                 </div>
-                <div class="col-6 border d-flex justify-content-center align-items-center">
+                <div class="col-6 d-flex justify-content-center align-items-center">
                     <ul class="nav">
                         <li class="nav-item"><a class="nav-link pages" href="#">Home</a></li>
                         <li class="nav-item"><a class="nav-link pages" href="#">Collection</a></li>
@@ -23,7 +27,11 @@
                         <li class="nav-item"><a class="nav-link pages" href="#">Contact</a></li>
                     </ul>
                 </div>
-                <div class="col-2 border"></div>
+                <div class="col-2  d-flex justify-content-end align-items-center">
+                    <a href="#"><i class="fa-solid me-3 fa-magnifying-glass header-icons"></i></a>
+                    <i class="fa-regular me-3 fa-user header-icons"></i>
+                    <a href="#"><i class="fa-solid fa-bag-shopping header-icons"></i></a>
+                </div>
             </div>
         </nav>
     </header>
--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php include "header.php" ?>
+<div class="container mt-2">
+    <div class="row border p-0 g-0">
+        <div class="col-6 d-flex flex-column justify-content-center align-items-start p-5">
+            <span class='text-uppercase'><i class="fa-solid fa-minus" style='color:#2A2A2A'></i>Our bestseller</span>
+            <h2 style='font-family: "Prata", serif;' class='my-2 fs-1'>Latest Arrival</h2>
+            <span class='text-uppercase'>Shop Now<i class="fa-solid fa-minus" style='color:#2A2A2A'></i></span>
+        </div>
+        <div class="col-6">
+            <img src="./images/header_img.png" class='img-fluid' alt="">
+        </div>
+    </div>
+</div>

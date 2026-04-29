4b4b7e8647a5f80e8b48a7a2b4d3a1ee0226d06d
ALI RAZA MUJAHID <razadeveloper816@gmail.com>
2026-04-29 11:11:20 -0700

    add best seller section

--- index.php
+++ index.php
@@ -1,13 +1,149 @@
 <?php include "header.php" ?>
-<div class="container mt-2">
-    <div class="row border p-0 g-0">
-        <div class="col-6 d-flex flex-column justify-content-center align-items-start p-5">
-            <span class='text-uppercase'><i class="fa-solid fa-minus" style='color:#2A2A2A'></i>Our bestseller</span>
-            <h2 style='font-family: "Prata", serif;' class='my-2 fs-1'>Latest Arrival</h2>
-            <span class='text-uppercase'>Shop Now<i class="fa-solid fa-minus" style='color:#2A2A2A'></i></span>
+<main>
+    <section class='header-section'>
+        <div class="container mt-2">
+            <div class="row border p-0 g-0">
+                <div class="col-6 d-flex flex-column justify-content-center align-items-start p-5">
+                    <span class='text-uppercase'><i class="fa-solid fa-minus" style='color:#2A2A2A'></i>Our bestseller</span>
+                    <h2 style='font-family: "Prata", serif;' class='my-2 fs-1'>Latest Arrival</h2>
+                    <span class='text-uppercase'>Shop Now<i class="fa-solid fa-minus" style='color:#2A2A2A'></i></span>
+                </div>
+                <div class="col-6">
+                    <img src="./images/header_img.png" class='img-fluid' alt="">
+                </div>
+            </div>
         </div>
-        <div class="col-6">
-            <img src="./images/header_img.png" class='img-fluid' alt="">
+    </section>
+    <section class='latest-collection-section'>
+        <div class="container">
+            <div class="row text-center my-5">
+                <div class="col-12">
+                    <h1 class='text-muted text-uppercase fs-2'>Latest <span class='fw-bold text-dark'>Collections</span><i class="fa-solid fa-minus" style='color:#2A2A2A'></i></h1>
+                    <span class='text-muted'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.</span>
+                </div>
+            </div>
+            <div class="row gy-4">
+                <div class="card col-3 p-0 rounded-0 border-0">
+                   <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark'>$149</span>
+                        </div>
+                   </div>
+                </div>
+                <div class="card col-3 p-0 rounded-0 border-0">
+                   <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark'>$149</span>
+                        </div>
+                   </div>
+                </div>
+                <div class="card col-3 p-0 rounded-0 border-0">
+                   <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark'>$149</span>
+                        </div>
+                   </div>
+                </div>
+                <div class="card col-3 p-0 rounded-0 border-0">
+                   <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark'>$149</span>
+                        </div>
+                   </div>
+                </div>
+                <div class="card col-3 p-0 rounded-0 border-0">
+                   <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark'>$149</span>
+                        </div>
+                   </div>
+                </div>
+                <div class="card col-3 p-0 rounded-0 border-0">
+                   <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark'>$149</span>
+                        </div>
+                   </div>
+                </div>
+                <div class="card col-3 p-0 rounded-0 border-0">
+                   <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark'>$149</span>
+                        </div>
+                   </div>
+                </div>
+                <div class="card col-3 p-0 rounded-0 border-0">
+                   <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark'>$149</span>
+                        </div>
+                   </div>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
+    </section>
+    <section class='best-seller-section'>
+        <div class="container">
+            <div class="row text-center my-5">
+                <div class="col-12">
+                    <h1 class='text-muted text-uppercase fs-2'>Best <span class='fw-bold text-dark'>Seller</span><i class="fa-solid fa-minus" style='color:#2A2A2A'></i></h1>
+                    <span class='text-muted'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.</span>
+                </div>
+            </div>
+            <div class="row gy-4">
+                <div class="card col-3 p-0 rounded-0 border-0">
+                   <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark'>$149</span>
+                        </div>
+                   </div>
+                </div>
+                <div class="card col-3 p-0 rounded-0 border-0">
+                   <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark'>$149</span>
+                        </div>
+                   </div>
+                </div>
+                <div class="card col-3 p-0 rounded-0 border-0">
+                   <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark'>$149</span>
+                        </div>
+                   </div>
+                </div>
+                <div class="card col-3 p-0 rounded-0 border-0">
+                   <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark'>$149</span>
+                        </div>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>

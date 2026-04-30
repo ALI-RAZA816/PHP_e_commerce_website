67136d9409878ed402cd7597f6baf60398b303b2
ALI RAZA MUJAHID <razadeveloper816@gmail.com>
2026-04-30 08:42:36 -0700

    Add filters sidebar and product sorting to collection page; enhance CSS for responsiveness and interactivity

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
                 <p class='text-muted fs-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             </div>
             <div class="col-lg-3 col-sm-3 p-0 ps-2 px-sm-0 mt-5 mt-sm-0">
-                <h2 class='text-uppercase fs-4 mb-3 fw-bold text-muted'>Company</h2>
+                <h2 class='text-uppercase fs-5 mb-3 fw-bold text-muted'>Company</h2>
                 <ul class="nav flex-column">
                     <li class="nav-item mb-0"><a class="nav-link footer-links mb-0" href="#">Home</a></li>
                     <li class="nav-item mb-0"><a class="nav-link footer-links mb-0" href="#">About Us</a></li>
@@ -15,7 +15,7 @@
                 </ul>
             </div>
             <div class="col-lg-3 col-sm-6 p-0 ps-2 px-sm-0 mt-5 mt-sm-0">
-                <h2 class='text-uppercase fs-4 mb-3 fw-bold text-muted'>Get in touch</h2>
+                <h2 class='text-uppercase fs-5 mb-3 fw-bold text-muted'>Get in touch</h2>
                 <p class='mb-2 text-muted'>+1-123-456-789</p>
                 <p class='mb-0 text-muted'>alirazamujahid102@gmail.com</p>
             </div>
--- js/script.js
+++ js/script.js
@@ -13,4 +13,12 @@ document.addEventListener('DOMContentLoaded',function(){
         header_nav_tab.classList.remove('active-menu');
         document.body.style.overflowY = 'auto';
     });
+
+
+    let filter_angle = document.getElementsByClassName('filter-angle')[0];
+    let filter_container = document.getElementById('filter-container');
+    filter_angle.addEventListener('click',function(){
+       filter_angle.classList.toggle('rotate');
+       filter_container.classList.toggle('expand');
+    });
 })

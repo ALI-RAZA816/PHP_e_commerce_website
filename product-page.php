affa278d72b075ad7c98409a3e8c7e48b0b93794
ALI RAZA MUJAHID <razadeveloper816@gmail.com>
2026-04-30 20:40:16 -0700

    Refactor product page layout; add size selection and related products sections

--- product-page.php
+++ product-page.php
@@ -1,63 +1,163 @@
 <?php include "header.php" ?>
-<div class="container">
-    <div class="row mt-5">
-        <div class="col-6">
-            <div class="row g-0 p-0">
-                <div class="col-2 me-4">
-                    <div class='mb-3'>
+<section class='product-images'>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-6">
+                <div class="row g-0 p-0">
+                    <div class="col-2 me-4">
+                        <div class='mb-3'>
+                            <img src="./images/Rectangle 3634.png" class='img-fluid' alt="">
+                        </div>
+                        <div class='mb-3'>
+                            <img src="./images/Rectangle 3634.png" class='img-fluid' alt="">
+                        </div>
+                        <div class='mb-3'>
+                            <img src="./images/Rectangle 3634.png" class='img-fluid' alt="">
+                        </div>
+                        <div>
+                            <img src="./images/Rectangle 3634.png" class='img-fluid' alt="">
+                        </div>
+                    </div>
+                    <div class="col-9 border">
                         <img src="./images/Rectangle 3634.png" class='img-fluid' alt="">
                     </div>
-                    <div class='mb-3'>
-                        <img src="./images/Rectangle 3634.png" class='img-fluid' alt="">
-                    </div>
-                    <div class='mb-3'>
-                        <img src="./images/Rectangle 3634.png" class='img-fluid' alt="">
-                    </div>
-                    <div>
-                        <img src="./images/Rectangle 3634.png" class='img-fluid' alt="">
-                    </div>
-                </div>
-                <div class="col-9 border">
-                    <img src="./images/Rectangle 3634.png" class='img-fluid' alt="">
                 </div>
             </div>
-        </div>
-        <div class="col-6">
-            <div class='product-detail'>
-                <h1 class='product-title text-dark fs-3'>Men Round Neck Pure Cotton T-shirt</h1>
-                <span class='product-price fs-4'>$139</span>
-                <p class='product-description mt-3 fs-6 '>A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.</p>
+            <div class="col-6">
+                <div class='product-detail'>
+                    <h1 class='product-title text-dark fs-3'>Men Round Neck Pure Cotton T-shirt</h1>
+                    <span class='product-price fs-4'>$139</span>
+                    <p class='product-description mt-3 fs-6 '>A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.</p>
+                </div>
+                <div class="product-size mt-4">
+                    <h3 class='fs-5 mb-3' style='color:#555555;'>Select Size</h3>
+                    <div class="me-4 form-check form-check-inline size-selection position-relative">
+                        <input class="form-check-input size-input" name='size' type="radio" id="inlineCheckbox1" value="S">
+                        <label class="form-check-label" for="inlineCheckbox1">S</label>
+                    </div>
+                    <div class="me-4 form-check form-check-inline size-selection">
+                        <input class="form-check-input size-input" name='size' type="radio" id="inlineCheckbox2" value="M">
+                        <label class="form-check-label" for="inlineCheckbox2">M</label>
+                    </div>
+                    <div class="me-4 form-check form-check-inline size-selection">
+                        <input class="form-check-input size-input" name='size' type="radio" id="inlineCheckbox3" value="L">
+                        <label class="form-check-label" for="inlineCheckbox3">L</label>
+                    </div>
+                    <div class="me-4 form-check form-check-inline size-selection">
+                        <input class="form-check-input size-input" name='size' type="radio" id="inlineCheckbox4" value="XL">
+                        <label class="form-check-label" for="inlineCheckbox4">XL</label>
+                    </div>
+                    <div class="me-4 form-check form-check-inline size-selection">
+                        <input class="form-check-input size-input" name='size' type="radio" id="inlineCheckbox5" value="XXL">
+                        <label class="form-check-label" for="inlineCheckbox5">XXL</label>
+                    </div>
+                </div>
+                <ul class="nav mt-5 border-top flex-column pt-3 ">
+                    <li class="nav-item mb-2" style='font-size:15px;color:#555555;'>100% Original product</li>
+                    <li class="nav-item mb-2" style='font-size:15px;color:#555555;'>Cash on delivery is available on this product.</li>
+                    <li class="nav-item" style='font-size:15px;color:#555555;'>Easy return and exchange policy within 7 days.</li>
+                </ul>
+                <button class='bg-dark text-white text-uppercase btn btn-dark rounded-0 mt-5' style='height:50px;'>Add to cart</button>
             </div>
-            <div class="product-size mt-4">
-                <h3 class='fs-5 mb-3' style='color:#555555;'>Select Size</h3>
-                <div class="me-4 form-check form-check-inline size-selection position-relative">
-                    <input class="form-check-input size-input" name='size' type="radio" id="inlineCheckbox1" value="S">
-                    <label class="form-check-label" for="inlineCheckbox1">S</label>
-                </div>
-                <div class="me-4 form-check form-check-inline size-selection">
-                    <input class="form-check-input size-input" name='size' type="radio" id="inlineCheckbox2" value="M">
-                    <label class="form-check-label" for="inlineCheckbox2">M</label>
-                </div>
-                <div class="me-4 form-check form-check-inline size-selection">
-                    <input class="form-check-input size-input" name='size' type="radio" id="inlineCheckbox3" value="L">
-                    <label class="form-check-label" for="inlineCheckbox3">L</label>
-                </div>
-                <div class="me-4 form-check form-check-inline size-selection">
-                    <input class="form-check-input size-input" name='size' type="radio" id="inlineCheckbox4" value="XL">
-                    <label class="form-check-label" for="inlineCheckbox4">XL</label>
-                </div>
-                <div class="me-4 form-check form-check-inline size-selection">
-                    <input class="form-check-input size-input" name='size' type="radio" id="inlineCheckbox5" value="XXL">
-                    <label class="form-check-label" for="inlineCheckbox5">XXL</label>
-                </div>
-            </div>
-           <ul class="nav mt-5 border-top flex-column pt-3 ">
-                <li class="nav-item mb-2" style='font-size:15px;color:#555555;'>100% Original product</li>
-                <li class="nav-item mb-2" style='font-size:15px;color:#555555;'>Cash on delivery is available on this product.</li>
-                <li class="nav-item" style='font-size:15px;color:#555555;'>Easy return and exchange policy within 7 days.</li>
-            </ul>
-            <button class='bg-dark text-white text-uppercase btn btn-dark rounded-0 mt-5' style='height:50px;'>Add to cart</button>
         </div>
     </div>
-</div>
+</section>
+<section class='product-description'>
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-12">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item rounded-0" role="presentation"><button style='width:120px;' class="nav-link rounded-0 border text-dark active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Description</button></li>
+                    <li class="nav-item" role="presentation"><button style='width:120px;' class="nav-link rounded-0 border text-dark" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Reviews</button></li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active border p-3" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+                        <p style='font-size:14px;color:#3a3a3a;font-weight:normal;'>An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.</p>
+                        <p style='font-size:14px;color:#3a3a3a;font-weight:normal;'>E-commerce websites typically display products or services along with detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant information.</p>
+                    </div>
+                    <div class="tab-pane fade border p-3" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
+                        <p style='font-size:14px;color:#3a3a3a;font-weight:normal;'>An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class='related-products' style='margin-bottom:10rem;'>
+    <div class="container mb-5">
+        <div class="row text-center my-5" style='margin:8rem 0;'>
+            <div class="col-12" style='margin:5rem 0;'>
+                <h1 class='text-muted text-uppercase fs-2'>Related <span class='fw-bold text-dark'>Products</span><i class="fa-solid fa-minus" style='color:#2A2A2A'></i></h1>
+                <span class='text-muted'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.</span>
+            </div>
+            <div class="card col-6 col-md-3 mb-5 mb-lg-0 col-lg-2 p-0 rounded-0 border-0">
+                <a href="product-page.php" class='text-decoration-none'>
+                    <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid image" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted d-inline-block'style='font-size:14px;text-align:left;'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark d-inline-block' style='text-align:left;'>$149</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="card col-6 col-md-3 mb-5 mb-lg-0 col-lg-2 p-0 rounded-0 border-0">
+                <a href="product-page.php" class='text-decoration-none'>
+                    <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid image" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted d-inline-block'style='font-size:14px;text-align:left;'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark d-inline-block' style='text-align:left;'>$149</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="card col-6 col-md-3 mb-5 mb-lg-0 col-lg-2 p-0 rounded-0 border-0">
+                <a href="product-page.php" class='text-decoration-none'>
+                    <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid image" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted d-inline-block'style='font-size:14px;text-align:left;'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark d-inline-block' style='text-align:left;'>$149</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="card col-6 col-md-3 mb-5 mb-lg-0 col-lg-2 p-0 rounded-0 border-0">
+                <a href="product-page.php" class='text-decoration-none'>
+                    <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid image" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted d-inline-block'style='font-size:14px;text-align:left;'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark d-inline-block' style='text-align:left;'>$149</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="card col-6 col-md-3 mb-5 mb-lg-0 col-lg-2 p-0 rounded-0 border-0">
+                <a href="product-page.php" class='text-decoration-none'>
+                    <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid image" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted d-inline-block'style='font-size:14px;text-align:left;'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark d-inline-block' style='text-align:left;'>$149</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="card col-6 col-md-3 mb-5 mb-lg-0 col-lg-2 p-0 rounded-0 border-0">
+                <a href="product-page.php" class='text-decoration-none'>
+                    <div class='px-3'>
+                        <img src="./images/Rectangle 3608.png" class="img-fluid image" alt="...">
+                        <div class="card-body p-0 d-flex flex-column">
+                            <span class='text-muted d-inline-block'style='font-size:14px;text-align:left;'>Women Round Neck Cotton Top</span>
+                            <span class='fw-bold text-dark d-inline-block' style='text-align:left;'>$149</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include "footer.php" ?>
 <!-- <script src='./js/size.js'></script> -->

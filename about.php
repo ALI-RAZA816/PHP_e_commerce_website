77aeb1ec82c305b9533811c8ed8d3050f2744474
ALI RAZA MUJAHID <razadeveloper816@gmail.com>
2026-05-03 16:31:28 -0700

    Update about page layout; add image and new content sections

--- about.php
+++ about.php
@@ -1,5 +1,5 @@
 <?php include "header.php" ?>
-<section class='about-us' style='min-height:100vh;'>
+<section class='about-us'>
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -7,7 +7,10 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-6">
+            <div class="col-4">
+                <img src="./images/about_img.png" class='img-fluid' alt="">
+            </div>
+            <div class="col-8">
                 <p class='text-muted' style='text-align:justify;'>Forever was born out of a passion for innovation and a desire to revolutionize the way people shop online. Our journey began with a simple idea: to provide a platform where customers can easily discover, explore, and purchase a wide range of products from the comfort of their homes.</p>
 
                 <p class='text-muted' style='text-align:justify;'>Since our inception, we've worked tirelessly to curate a diverse selection of high-quality products that cater to every taste and preference. From fashion and beauty to electronics and home essentials, we offer an extensive collection sourced from trusted brands and suppliers.</p>
@@ -16,7 +19,7 @@
 
                 <p class='text-muted' style='text-align:justify;'>Our mission at Forever is to empower customers with choice, convenience, and confidence. We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.</p>
             </div>
-            <div class="col-6">
+            <!-- <div class="col-6">
                 <form action="" class='about-us-form'>
                     <div class='mb-3'>
                         <input type="text" class='form-control rounded-0' placeholder='Enter name'>
@@ -29,8 +32,41 @@
                     </div>
                     <button class='text-white bg-dark rounded-0 d-block w-100 btn mt-3'>Submit</button>
                 </form>
+            </div> -->
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h5 class='text-muted text-uppercase my-5'>Why <span class='fw-bold text-dark'>Choose Us</span><i class="fa-solid fa-minus" style='color:#2A2A2A'></i></h5>
             </div>
         </div>
+        <div class="row">
+            <div class="col-4 p-5 border">
+                <p class='mb-4 fw-bold text-uppercase'>Quality Assurance:</p>
+                <p class='m-0 text-muted'>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+            </div>
+            <div class="col-4 p-5 border">
+                <p class='mb-4 fw-bold text-uppercase'>Convenience:</p>
+                <p class='m-0 text-muted'>With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
+            </div>
+            <div class="col-4 p-5 border">
+                <p class='mb-4 fw-bold text-uppercase'>Exceptional Customer Service:</p>
+                <p class='m-0 text-muted'>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.</p>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+                <div class="col-md-6 col-lg-6 text-center" style='margin:10rem 0;'>
+                    <h2>Subscribe now & get 20% off</h2>
+                    <p class='text-muted'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                    <div class="subscription d-flex">
+                        <input type="text" placeholder='Enter your email address' class='form-control rounded-0'>
+                        <button class='text-white bg-dark btn rounded-0 text-uppercase'>Subscribe</button>
+                    </div>
+                </div>
+            </div>
     </div>
 </section>
 <?php include "footer.php" ?>

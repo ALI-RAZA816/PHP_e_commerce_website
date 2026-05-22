9f76991669560ae9b56d0adb55d45574268cb339
ALI RAZA MUJAHID <razadeveloper816@gmail.com>
2026-05-22 08:59:44 -0700

    Add new product images to the admin image directory
    - Added four new images for product representation:
      - 1779464939-ewerx-malta-gordos-VwyUF2xdBh0-unsplash.jpg
      - 1779464939-ewrdsf-ewerx-malta-gordos-VwyUF2xdBh0-unsplash - Copy.jpg
      - 1779464939-ppsd-fsewerx-malta-gordos-VwyUF2xdBh0-unsplash - Copy.jpg
      - 1779464939-ssdrwe-ewerx-malta-gordos-VwyUF2xdBh0-unsplash - Copy.jpg

--- admin/script/cart-total.php
+++ admin/script/cart-total.php
@@ -2,16 +2,23 @@
 
     include "config.php";
     $SHIPPING_FEE = 1;
-    $query = "SELECT SUM(quantity * price) AS total FROM cart";
-    $result = mysqli_query($conn, $query);
-    if(mysqli_num_rows($result) > 0){
-        $row = mysqli_fetch_assoc($result);
-        $subtotal = $row['total'] + $SHIPPING_FEE;
-        $output = "<p class='mb-2 d-flex justify-content-between border-bottom pb-2'><span>Subtotal</span><span><span>$</span>{$row['total']}.00</span></p>
-        <p class='mb-2 d-flex justify-content-between border-bottom pb-2'><span>Shipping Fee</span><span><span>$</span> {$SHIPPING_FEE}.00</span></p>
-        <p class='mb-2 d-flex justify-content-between border-bottom pb-2'><span class='fw-bold'>Total</span><span><span>$</span>{$subtotal}.00</span></p>";
-        echo $output;
-        }else{
+    session_start();
+    if(isset($_SESSION['name'])){
+        $query = "SELECT SUM(quantity * price) AS total FROM cart WHERE user = '{$_SESSION['name']}'";
+        $result = mysqli_query($conn, $query);
+        if(mysqli_num_rows($result) > 0){
+            $row = mysqli_fetch_assoc($result);
+            $subtotal = $row['total'] + $SHIPPING_FEE;
+            $output = "<p class='mb-2 d-flex justify-content-between border-bottom pb-2'><span>Subtotal</span><span><span>$</span>{$row['total']}.00</span></p>
+            <p class='mb-2 d-flex justify-content-between border-bottom pb-2'><span>Shipping Fee</span><span><span>$</span> {$SHIPPING_FEE}.00</span></p>
+            <p class='mb-2 d-flex justify-content-between border-bottom pb-2'><span class='fw-bold'>Total</span><span><span>$</span>{$subtotal}.00</span></p>";
+            echo $output;
+            }else{
+                $output = "<p class='mb-2 d-flex justify-content-between border-bottom pb-2'><span>Subtotal</span><span>$00.00</span></p>
+                <p class='mb-2 d-flex justify-content-between border-bottom pb-2'><span>Shipping Fee</span><span><span>$</span> {$SHIPPING_FEE}.00</span></p>
+                <p class='mb-2 d-flex justify-content-between border-bottom pb-2'><span class='fw-bold'>Total</span><span>$00.00</span></p>";
+                }
+    }else{
             $output = "<p class='mb-2 d-flex justify-content-between border-bottom pb-2'><span>Subtotal</span><span>$00.00</span></p>
             <p class='mb-2 d-flex justify-content-between border-bottom pb-2'><span>Shipping Fee</span><span><span>$</span> {$SHIPPING_FEE}.00</span></p>
             <p class='mb-2 d-flex justify-content-between border-bottom pb-2'><span class='fw-bold'>Total</span><span>$00.00</span></p>";
